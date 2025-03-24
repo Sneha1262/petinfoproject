11ab0baa03353b23e35cfdfbd4786fb5b7677b94
Sneha1262 <sne6124@gmail.com>
2025-03-24 15:54:59 +0000

    allowd hosts

--- petproject/petproject/settings.py
+++ petproject/petproject/settings.py
@@ -128,4 +128,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['34.224.174.119', 'http://127.0.0.1:8000/']
