0365c7f0c47bca9fadebf3c1750dc745c0bc575b
Sneha1262 <sne6124@gmail.com>
2025-04-01 19:53:13 +0100

    changes

--- petproject/petproject/urls.py
+++ petproject/petproject/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('api/', include('pets.urls')),
+    path('', home_view),
+
 ]
