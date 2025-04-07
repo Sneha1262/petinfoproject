78508f58967b693c3c402593e12fe3834cf0ec22
Sneha1262 <sne6124@gmail.com>
2025-04-08 00:54:07 +0100

    ip

--- petproject/petproject/settings.py
+++ petproject/petproject/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#ALLOWED_HOSTS =['35.173.214.218','http://127.0.0.1:8000/api/pets/']
+#ALLOWED_HOSTS =['44.211.8.119','http://127.0.0.1:8000/api/pets/']
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [    'http://127.0.0.1:8000',]
